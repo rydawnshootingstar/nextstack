a3de8ccc9d4de30057accb11c2b4968649f5efe2
Ryan <rydawnshootingstar@gmail.com>
2021-02-18 22:39:54 -0500

    again line endings? added postgres heroku server and sequelize

---
+++ models/index.js
@@ -0,0 +1,30 @@
+// model/index.js
+import Sequelize from "sequelize";
+import userModel from "./user";
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        operatorAliases: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+    }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+//...
+
+db.users = userModel(sequelize, Sequelize);
+
+export default db;
---
+++ pages/api/user.js
@@ -0,0 +1,37 @@
+// pages/api/user.js
+import isEmail from 'validator/lib/isEmail'
+import isLength from 'validator/lib/isLength'
+import db from '../../models/index';
+db.sequelize.sync()
+const User = db.users
+
+export default async (req, res) => {
+    // console.log(req.body)
+    // Recieved params from request
+    let { name, email } = req.body
+    try {
+        console.log(name, email);
+        // check email, name, password format
+        if (!isLength(name, {min: 3, max: 15})) {
+            return res.status(422).send("Name must be 3-10 characters long");
+        } else if (!isEmail(email)){
+            return res.status(422).send("Email must be valid");
+        }
+
+        // Check if user with that email if already exists
+        const user = await User.findOne({
+            where: { email: email }
+        })
+        if(user){
+            return res.status(422).send(`User already exist with that ${email}`)
+        }
+        const newUser = await User.create({
+            name, 
+            email
+        })
+        res.status(201).send(newUser)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Error in signup. Please try again.")
+    }
+}
