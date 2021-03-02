9675ca918ec5b883ca068dd0ce886a9b60513a8e
rydawnshootingstar <rydawnshootingstar@gmail.com>
2021-03-02 11:49:35 -0500

    commit before cloning

--- models/index.js
+++ models/index.js
@@ -1,23 +1,22 @@
 // model/index.js
-import Sequelize from "sequelize";
-import userModel from "./user";
+import Sequelize from 'sequelize';
+import userModel from './user';
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: "postgres",
-        operatorAliases: false,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
-);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+	host: process.env.DB_HOST,
+	dialect: 'postgres',
+	operatorAliases: false,
+	ssl: true,
+	dialectOptions: {
+		ssl: { requre: true, rejectUnauthorized: false },
+	},
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+});
 
 const db = {};
 
